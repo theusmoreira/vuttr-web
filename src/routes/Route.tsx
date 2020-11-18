52348df4ffeca26400dfe6e2a9537ac9f740d970
Matheus Santos <matheussm301@gmail.com>
2020-11-18 13:26:31 -0300

    feat: create routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Route as ReactDomRouter,
+  RouteProps as ReactDomRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/Auth';
+
+interface RouteProps extends ReactDomRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDomRouter
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/tools',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Route from './Route';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+
+      {/* <Route path="/tools" component={Dashboard} isPrivate /> */}
+    </Switch>
+  );
+};
+
+export default Routes;
