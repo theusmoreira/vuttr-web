980586abc03039a0b48af7f3c99ba54eb3dad136
Matheus Santos <matheussm301@gmail.com>
2020-11-17 22:24:58 -0300

    feat: add styles global

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,1 +1,34 @@
-import {} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    overflow: hidden;
+    font-family: Source sans pro, sans-serif;
+    background: #fdfdfd;
+    color: #000;
+    -webkit-font-smoothing: antialiased;
+  }
+
+ border-style, input, button, textarea, label {
+    font-family: Source sans pro, sans-serif;
+    font-size: 22px;
+
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
