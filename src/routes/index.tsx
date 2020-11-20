a1f1d92a40d6e8c5aa9105101d1e816c65e37b94
Matheus Santos <matheussm301@gmail.com>
2020-11-19 22:49:15 -0300

    chore: add dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 import Route from './Route';
 
@@ -12,7 +13,7 @@ const Routes: React.FC = () => {
       <Route exact path="/" component={SignIn} />
       <Route path="/signup" component={SignUp} />
 
-      {/* <Route path="/tools" component={Dashboard} isPrivate /> */}
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
